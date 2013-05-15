050ce94ee31cdaf7c538de1aa2e38fb776446ca4
GuoXu <ggxx120@gmail.com>
2013-05-15 22:33:44 +0800

    daily commit  20130515b

--- src/App/src/com/ss/stg/SubtourEditDialogFragment.java
+++ src/App/src/com/ss/stg/SubtourEditDialogFragment.java
@@ -1,7 +1,40 @@
 package com.ss.stg;
 
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SubtourEditDialogFragment extends DialogFragment {
-	
+
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+	}
+
+	public SubtourEditDialogFragment() {
+		super();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup view, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.dialog_subtour_item, view);
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return super.onCreateDialog(savedInstanceState);
+	}
 }
